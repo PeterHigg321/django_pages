402996966c5abfe7eb16ef6e5b18da9c82cdecce
Peter Higginbotham <phiggi3@lsu.edu>
2023-04-04 12:44:32 -0500

    Heroku update stuff

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e-k!pvz5tjyri0xjy03oozc^-+mob=jtn)u#i!sdqvu&s7=#tx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
